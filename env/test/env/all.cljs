b360dcf00393514ba5119a3f37586e5dc7cb5031
BJ Neilsen <bj.neilsen@gmail.com>
2017-06-16 14:44:26 -0600

    Get make test working

--- env/test/env/all.cljs
+++ env/test/env/all.cljs
@@ -2,12 +2,15 @@
 
 (enable-console-print!)
 
+;; We need to babelify certain plugins that use "import" et al
+((js/require "babel-register") (clj->js {:only #".+/node_modules/apsl-react-native-button.*"}))
+
 (js/require "react-native-mock/mock")
-(def ReactNative (js/require "react-native"))
+(def react-native (js/require "react-native"))
 
 ;; Some rn components are not mocked correctly
-(aset (.-Header (.-NavigationExperimental ReactNative)) "Title" "")
-(aset ReactNative "SwipeableListView" (clj->js {}))
+(aset (.-Header (.-NavigationExperimental react-native)) "Title" "")
+(aset react-native "SwipeableListView" (clj->js {}))
 
 ;; XMLHttpRequest is not available in nodejs target
 ;; Use a npm package.
--- test/brewsky/db_test.cljs
+++ test/brewsky/db_test.cljs
@@ -1,7 +1,7 @@
 (ns brewsky.db-test
   (:require [cljs.test :refer [deftest is are testing run-tests]]
-            [brewsky.db :refer :all]))
+            [brewsky.db :as db]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 1 0))))
+    (is (= 1 1))))
