2baf7149cc7bb31177d41697da50ffb16d31dacf
BJ Neilsen <bj.neilsen@gmail.com>
2017-08-30 13:27:13 +0200

    Trying to get testing working again

--- env/test/env/all.cljs
+++ env/test/env/all.cljs
@@ -5,9 +5,9 @@
 ;; We need to babelify certain plugins that use "import" et al
 ((js/require "babel-register") (clj->js {:only #".+/node_modules/apsl-react-native-button.*"}))
 
-; (js/require "react-native-mock/mock")
+(js/require "react-native-mock/mock")
 (def react-native (js/require "react-native"))
 
 ;; Some rn components are not mocked correctly
-(aset (.-Header (.-NavigationExperimental react-native)) "Title" "")
+; (aset (.-Header (.-NavigationExperimental react-native)) "Title" "")
 (aset react-native "SwipeableListView" (clj->js {}))
