b87db57b42aeb037550b3fde6177d7b22f5a7f6b
BJ Neilsen <bj.neilsen@gmail.com>
2017-09-05 12:13:51 +0100

    Log changes around all event handlers for now

--- src/brewsky/app/events.cljs
+++ src/brewsky/app/events.cljs
@@ -1,11 +1,14 @@
 (ns brewsky.app.events
   (:require
-   [brewsky.app.navigation.events :as navigation]
-   [brewsky.app.recipes.create.events :as recipes-create]
-   [brewsky.app.db :as db :refer [app-db]]
-   [clojure.spec.alpha :as s]
-   [re-frame.core :refer [reg-event-db after debug]]
-   ))
+    [brewsky.app.db :as db :refer [app-db]]
+    [brewsky.app.navigation.events :as navigation]
+    [brewsky.app.recipes.create.events :as recipes-create]
+    [clojure.data :as data]
+    [clojure.spec.alpha :as s]
+    [re-frame.core :refer [reg-event-db after]]
+    [re-frame.interceptor :refer [->interceptor get-effect get-coeffect]]
+    [re-frame.loggers :refer  [console]]
+    ))
 
 ;; -- Interceptors ------------------------------------------------------------
 ;;
@@ -34,6 +37,45 @@
           (.error js/console e.stack)   ;; print a sane stacktrace
           (throw e))))))
 
+(def log-around
+  "An interceptor which logs/instruments an event handler's actions to
+  `js/console.debug`. See examples/todomvc/src/events.cljs for use.
+  Output includes:
+  1. the event vector
+  2. a `clojure.data/diff` of db, before vs after, which shows
+  the changes caused by the event handler.  You will absolutely have
+  to understand https://clojuredocs.org/clojure.data/diff to
+  understand the output.
+  You'd typically include this interceptor after (to the right of) any
+  path interceptor.
+  "
+  (when ^boolean goog.DEBUG
+    (->interceptor
+      :id     :log-around
+      :before (fn log-around-before
+                [context]
+                (console :log "Handling re-frame event:" (-> context (get-coeffect :event) first clj->js))
+                context)
+      :after  (fn log-around-after
+                [context]
+                (let [event (get-coeffect context :event)
+                      event-name (-> event first clj->js)
+                      event-args (rest event)
+                      orig-db (get-coeffect context :db)
+                      new-db  (get-effect   context :db ::not-found)]
+                  (if (= new-db ::not-found)
+                    (console :log "No :db changes caused by:" event-name)
+                    (let [[only-before only-after] (data/diff orig-db new-db)
+                          db-changed?    (or (some? only-before) (some? only-after))
+                          diff {:changed db-changed? :before only-before :after only-after}]
+                      (do (console :group "db clojure.data/diff for:" event-name)
+                          (console :log "[event]" event-name)
+                          (console :log "[event args]" (clj->js event-args))
+                          (console :log "[new db]" (clj->js new-db))
+                          (console :log "[diff]" (clj->js diff))
+                          (console :groupEnd))))
+                  context)))))
+
 ;; -- Handlers --------------------------------------------------------------
 
 (defn initialize-db
@@ -46,10 +88,10 @@
         (.error js/console conform-explanation))
       app-db)))
 
-(reg-event-db :initialize-db [debug validate-spec] initialize-db)
-(reg-event-db :navigation/pop-scene [debug validate-spec] navigation/pop-scene)
-(reg-event-db :navigation/push-scene [debug validate-spec] navigation/push-scene)
-(reg-event-db :navigation/replace-scenes [debug validate-spec] navigation/replace-scenes)
-(reg-event-db :recipes-create/create-recipe-partial [debug validate-spec] recipes-create/create-recipe-partial)
-(reg-event-db :recipes-create/title-changed [debug validate-spec] recipes-create/title-changed)
-(reg-event-db :recipes-create/style-changed [debug validate-spec] recipes-create/style-changed)
+(reg-event-db :initialize-db [log-around validate-spec] initialize-db)
+(reg-event-db :navigation/pop-scene [log-around validate-spec] navigation/pop-scene)
+(reg-event-db :navigation/push-scene [log-around validate-spec] navigation/push-scene)
+(reg-event-db :navigation/replace-scenes [log-around validate-spec] navigation/replace-scenes)
+(reg-event-db :recipes-create/create-recipe-partial [log-around validate-spec] recipes-create/create-recipe-partial)
+(reg-event-db :recipes-create/title-changed [log-around validate-spec] recipes-create/title-changed)
+(reg-event-db :recipes-create/style-changed [log-around validate-spec] recipes-create/style-changed)
